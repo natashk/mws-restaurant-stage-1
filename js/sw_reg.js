a019211a546ff7158141f9162ade937ea1020f56
unknown <box240308@yandex.ru>
2018-10-02 19:29:12 -0400

    feat: use ServiceWorker to cache requests to all of the site’s assets

---
+++ js/sw_reg.js
@@ -0,0 +1,9 @@
+if (!navigator.serviceWorker) {
+
+  navigator.serviceWorker.register("../sw.js").then(function(reg) {
+    if (!navigator.serviceWorker.controller) {
+      return;
+    }
+
+  });
+}
---
+++ sw.js
@@ -0,0 +1,72 @@
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("mvs_restaurant1").then(function(cache) {
+      return cache.addAll([
+        "js/dbhelper.js",
+        "js/main.js",
+        "js/restaurant_info.js",
+        "css/styles.css",
+        "index.html",
+        "data/restaurants.json",
+        "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+        "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('index.html'));
+      return;
+    }
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(fetchImage(event.request));
+      return;
+    }
+    if (requestUrl.pathname.startsWith('/restaurant.html')) {
+      event.respondWith(fetchRestaurantPage(event.request));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  ); 
+
+});
+
+function fetchImage(request) {
+  var url = request.url;
+
+  return caches.open("mvs_restaurant1").then(function(cache) {
+    return cache.match(url).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(url, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+ 
+function fetchRestaurantPage(request) {
+  var url = request.url;
+
+  return caches.open("mvs_restaurant1").then(function(cache) {
+    return cache.match(url).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(url, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
